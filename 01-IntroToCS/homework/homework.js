97298f0b5e692cfb46352f997c2442d1f5c38fef
Juan Francisco Drewanz <quico@MacBook-Pro-de-Juan.local>
2022-07-18 15:51:18 -0300

    Primera tarea

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -2,11 +2,27 @@
 
 function BinarioADecimal(num) {
   // tu codigo aca
+  let str = '';
+  let arreglo = (str+num).split('').reverse();
+  let decimal = 0;
+
+  for (let i = 0; i < arreglo.length; i++){
+      decimal+= Math.pow(2,i)*arreglo[i];
+  }
   
+  return decimal;
+
 }
 
 function DecimalABinario(num) {
   // tu codigo aca
+  let binario = [];
+  while (num>0) {
+    binario.unshift((num%2));
+    num = Math.floor(num/2);
+  }
+  
+  return binario.join('');
 
 }
 
